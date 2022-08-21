04c3bfabc7d92167b1ee0e3f835f9be39af41092
mohdadilvp <mohdadilvp@gmail.com>
2022-08-21 10:56:32 +0200

    Trengo test

---
+++ tests/Unit/TrengoTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use Illuminate\Support\Facades\Config;
+
+class TrengoTest extends TestCase
+{
+	protected $apiKey;
+
+	protected function setUp(): void
+	{
+	    parent::setUp();
+	    $this->apiKey = env('TRENGO_API_KEY', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiNDRiY2ZiMWJiYTlkNjUzNjJlM2NmODc1ZWY1OTNhY2NlMzE4NmEyMTVhYTM0YTE5YTE2YWQ2Mjk0ZWU1YmRmYmQzYzYzZDBmMzI2ZDRlODMiLCJpYXQiOjE2NjA5NTQxNDYuODk3NTgyLCJuYmYiOjE2NjA5NTQxNDYuODk3NTg0LCJleHAiOjQ3ODUwOTE3NDYuODkyMDUyLCJzdWIiOiI1MTcwODQiLCJzY29wZXMiOltdfQ.K7oVWkSAdTMI6dcb3tQBHg6OHfw5pHad0Di51Bt9-KwVZugjI_ysbPofJ314jna6v6NMX_8Nx5Ehz1dBJyF7sw');
+
+	}
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_trengo()
+    {
+    	$trengo = new \App\Utils\Trengo($this->apiKey);
+    	$response = $trengo->getCustomFields();
+    	$this->assertArrayHasKey('response', $response);
+    }
+}
